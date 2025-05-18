fcbb3db68ece8442a5fb9d74f1f36cd2d08e2309
Aritra Roy <71834518+lucifer206@users.noreply.github.com>
2025-05-18 18:01:30 +0530

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
 
-  if (user === "GF" && pass === "IloveYou") {
-    window.location.href = "gf.html";
-  } else if (user === "Admin" && pass === "God") {
     window.location.href = "admin.html";
   } else {
     alert("🚫 Invalid credentials! Try again, sweetie 💔");
